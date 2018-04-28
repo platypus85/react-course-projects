7ca87ef1814f91d6b73c826488e2e0730dd53888
Alberto Camillo <info@albertocamillo.com>
2018-04-28 22:27:35 +1000

    JSX Expressions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,11 +3,71 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
+
+var article = {
+		title: 'Indecision App',
+		subTitle: 'Put your life in the hands of a computer'
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision app'
+		'div',
+		null,
+		React.createElement(
+				'h1',
+				null,
+				article.title
+		),
+		React.createElement(
+				'p',
+				null,
+				article.subTitle
+		),
+		React.createElement(
+				'ol',
+				null,
+				React.createElement(
+						'li',
+						null,
+						'Item One'
+				),
+				React.createElement(
+						'li',
+						null,
+						'Item Two'
+				)
+		)
 );
+var user = {
+		name: 'Alberto',
+		age: 33,
+		location: 'Sydney'
+};
+var userName = 'Alberto';
+var userAge = 33;
+var userLocation = 'Sydney';
+var templateTwo = React.createElement(
+		'div',
+		null,
+		React.createElement(
+				'h1',
+				null,
+				'Name: ',
+				user.name
+		),
+		React.createElement(
+				'p',
+				null,
+				'Age: ',
+				user.age
+		),
+		React.createElement(
+				'p',
+				null,
+				'Location: ',
+				user.location
+		)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,38 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision app</h1>;
+
+var article = {
+		title: 'Indecision App',
+		subTitle: 'Put your life in the hands of a computer'
+}
+
+var template = (
+		<div>
+				<h1>{article.title}</h1>
+				<p>{article.subTitle}</p>
+				<ol>
+						<li>Item One</li>
+						<li>Item Two</li>
+				</ol>
+		</div>
+);
+var user = {
+		name: 'Alberto',
+		age: 33,
+		location: 'Sydney'
+}
+var userName = 'Alberto';
+var userAge = 33;
+var userLocation = 'Sydney';
+var templateTwo = (
+		<div>
+				<h1>Name: {user.name}</h1>
+				<p>Age: {user.age}</p>
+				<p>Location: {user.location}</p>
+		</div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot)
