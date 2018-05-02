79053bd23cb28fb020af2797b39507650124690b
Alberto Camillo <alberto.camillo@terem.com.au>
2018-05-02 22:56:44 +1000

    Events and Attributes

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,39 +1,91 @@
 'use strict';
 
-var add = function add(a, b) {
-    console.log(arguments); //No more allowed in ES6
-    return a + b;
+console.log('App.js is running');
+
+// JSX - JavaScript XML
+
+var app = {
+    title: 'Indecision App',
+    subTitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
-console.log(add(55, 1));
 
-var addArrow = function addArrow(a, b) {
-    return a + b;
-};
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    getSubtitle(app.subTitle),
+    app.options && app.options.length > 0 ? React.createElement(
+        'p',
+        null,
+        'Here are your options:'
+    ) : 'No options',
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
+);
 
-//this keyword = no longer bound in Arrow function.
-
-var user = {
-    name: 'John',
-    cities: ['Sydney', 'Perth', 'Darwin'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
+function getSubtitle(subtitle) {
+    if (subtitle) {
+        return React.createElement(
+            'p',
+            null,
+            subtitle
+        );
     }
-};
-console.log(user.printPlacesLived());
+}
 
-var multiplier = {
-    numbers: [10, 20, 30],
-    multiplyBy: 3,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
-    }
+var count = 0;
+var addOne = function addOne() {
+    console.log('addOne');
 };
-console.log(multiplier.multiply());
+var minusOne = function minusOne() {
+    console.log('minusOne');
+};
+var reset = function reset() {
+    console.log('reset');
+};
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Count: ',
+        count
+    ),
+    React.createElement(
+        'button',
+        { onClick: addOne },
+        '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'Reset'
+    )
+);
+console.log(templateTwo);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
