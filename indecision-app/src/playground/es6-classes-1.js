03e3aef82c25d2c208559c5dcf801503b577cc66
Alberto Camillo <info@albertocamillo.com>
2018-05-04 23:43:39 +1000

    Classes part 2

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -4,18 +4,50 @@ class Person {
         this.age = age;
     }
     getGreeting(){
-        //return 'Hi, I am ' + this.name + '!';
         return `Hi, my name is ${this.name}!`
     }
     getDescription(){
-        return `${this.name} is ${this.age} years old.`
+        return `${this.name} is ${this.age} years old.`;
     }
 }
 
-const john = new Person('John Doe', 45);
-//console.log(john.getGreeting());
-console.log(john.getDescription());
+class Student extends Person{
+    constructor(name, age, major){
+        super(name, age); //Calling parent class constructor
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major;
+    }
+    getDescription(){
+        let description = super.getDescription();
+        if(this.hasMajor()){
+            description += ` His major is: ${this.major}`;
+        }
+        return description;
+    }
+}
 
-const other = new Person();
+class Traveler extends Person{
+    constructor(name, age, homeLocation){
+        super(name, age); //Calling parent class constructor
+        this.homeLocation = homeLocation;
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+        if(this.homeLocation){
+            greeting += ` I am visiting from ${this.homeLocation}`; 
+        }
+        return greeting;
+    }
+}
+
+const john = new Traveler('John Doe', 45, 'Sydney');
+console.log(john);
+//console.log(john.hasMajor());
+console.log(john.getGreeting());
+
+const other = new Traveler();
+console.log(other);
+//console.log(other.hasMajor());
 console.log(other.getGreeting());
-//console.log(other.getDescription());
