61f16ca22c118c6f0088ee7fde0aee67310624c9
Alberto Camillo <info@albertocamillo.com>
2018-05-26 20:12:37 +1000

    Component lifecycle

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -11,6 +11,18 @@ class IndecisionApp extends React.Component{
         }
     }
 
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+    }
+
+    componentWillUnmount(){
+        console.log('componentWillUnmount');
+    }
+    
     handleDeleteOptions(){
         this.setState(()=>({options: []}));
     }
